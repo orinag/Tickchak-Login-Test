febfa588b10fce4ce5029c6b8b89b431088d05fd
Ori Nagar <ori.ngr@gmail.com>
2023-02-19 15:38:32 +0200

    cleaning code

--- cypress/e2e/sign-up-test.js
+++ cypress/e2e/sign-up-test.js
@@ -10,7 +10,7 @@ describe("Login page test for TikChak", () => {
       },
 
       {
-        fixture: "../fixtures/example.json",
+        fixture: "../fixtures/mocking-sms.json",
       }
     );
   });
